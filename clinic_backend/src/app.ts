6f44edf9de36cd6af23b1b45050dcca7ec99fa76
reyhanhmdani <rey7dan7@gmail.com>
2026-08-12 10:33:32 +0700

    feat(backend): add cors middleware

--- clinic_backend/src/app.ts
+++ clinic_backend/src/app.ts
@@ -2,10 +2,17 @@ import express from 'express';
 import { logger } from './middlewares/logger.js';
 import mainRouter from './routes/index.js';
 import { errorMiddleware } from './middlewares/errorMiddleware.js';
+import cors from 'cors';
 
 const app = express();
 const PORT = 3000;
 
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
